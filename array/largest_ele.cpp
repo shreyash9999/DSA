ee23713fe8ad053eaa3b61644b771dfdef620003
shreyash9999 <dongareshreyash20@gmail.com>
2023-06-28 19:49:10 +0530

    second_element

--- array/largest_ele.cpp
+++ array/largest_ele.cpp
@@ -25,19 +25,18 @@ int largest(int arr[],int n){
 
 
 int secondlargest(int arr[],int n){
-    int res = -1,largest  =0;
+    int first = 0,second  =-1;
     for (int i=1;i<n;i++){
-        if(arr[i]>arr[largest]){
-            res= largest;
-            largest = i;
+        if(arr[i]>arr[first]){
+            second= first;
+            first = i;
         }
-        else if (arr[i]!= arr[largest]){
-            if(res==-1 || arr[i]>arr[res]){
-                res = i;
+        else if (arr[i]!= arr[first]){
+            if(second==-1 || arr[second]<arr[i]){
+                second = i;
             }
         }
-        return res;
-    }
+    }return arr[second];
 }
 
 int main(){
