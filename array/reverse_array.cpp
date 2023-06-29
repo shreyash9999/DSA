d9656ca9461ed8360a3dd447401f3b529149ce1d
shreyash9999 <dongareshreyash20@gmail.com>
2023-06-29 17:46:54 +0530

    fun rever

--- array/reverse_array.cpp
+++ array/reverse_array.cpp
@@ -2,6 +2,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int reverse(int arr[],int n){
+    int low=0,high =n-1;
+    while(low<high){
+        int temp = arr[low];
+        arr[low]=arr[high];
+        arr[high]= temp;
+        low++;
+        high--;
+    }
+}//implement the function (currently:not)
 
 int main(){
     int arr[] = {1,2,3,4,5,6};
