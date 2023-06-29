8501ce8251490ad116882c3b7b600218674a1c9f
shreyash9999 <dongareshreyash20@gmail.com>
2023-06-29 18:29:37 +0530

    secondlargerst update

--- array/second_largest_using_vector.cpp
+++ array/second_largest_using_vector.cpp
@@ -2,6 +2,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+	int print2largest(int arr[], int n) {
+	    // code here
+        // vector<int> v(begin(arr),end(arr));
+        vector<int> v;
+        v.assign(arr,arr+n);
+        
+        set<int> s(v.begin(),v.end());
+        
+        v.clear();
+        
+        for (auto it:s) v.push_back(it);
+        int a = v.size();
+        
+        if(v[a-2]==0){
+            return -1;
+        }else{
+            return v[a-2];
+        };
+	}//if array is given then you have to convert it to vector then set then empty v then add in v then -2 by size
+
 int main(){
     vector<int> v{2,43,54,67,21,90};
 
