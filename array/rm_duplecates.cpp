b9f264b86037e1a2487ae41a5a80c009d693b4c7
shreyash9999 <dongareshreyash20@gmail.com>
2023-07-03 00:46:03 +0530

    almost complet

---
+++ array/rm_duplecates.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int rm_dup(int arr[],int n){
+    vector<int> v;
+    v.assign(arr,arr+n);
+
+    set<int> s(v.begin(),v.end());
+    for(auto it:s)cout<<it<<" "<<endl;
+    cout<<s.size();
+}
+
+int main(){
+    int arr[]={10,20,20,30,30,30};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    rm_dup(arr,n);
+    return 0;
+}
